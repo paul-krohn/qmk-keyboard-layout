e2729c9022b99343083ffd578c349986424b94a5
Paul Krohn <pkrohn@daemonize.com>
2020-08-18 15:52:01 -0700

    make no-pattern key more accessible

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ When you want to build your firmware, you can run:
 qmk compile --keyboard v60_type_r --keymap starry-night-custom
 ```
 from anywhere, which will drop a new `v60_type_r_starry-night-custom.hex` in the root of `~/qmk_firmware`, which you can flash your keyboard with.
+
+```
+qmk compile --keyboard massdrop/alt --keymap paul
+```
--- drop-alt-custom/keymap.c
+++ drop-alt-custom/keymap.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
         _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
-        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
-        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END \
+        _______, RGB_TOG, RGB_M_P, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, _______,          KC_F14, KC_VOLD, \
+        _______, _______, _______,                            _______,                            _______, _______, KC_KP_MINUS, KC_KP_PLUS, KC_KP_ENTER \
     ),
     [2] = LAYOUT(
        /* There are a few keys that can be made more Mac-like: brightness up/down (f1/f2), media previous (f7), play/pause (f8), and media next (f9)
