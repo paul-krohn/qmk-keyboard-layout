e5b58ab7838f0f8cb2fa5e2469054fdad134f371
Paul Krohn <pkrohn@daemonize.com>
2022-01-17 20:52:42 -0800

    replace home with ctrl-alt combo

--- drop-alt-custom/keymap.c
+++ drop-alt-custom/keymap.c
@@ -17,27 +17,27 @@ keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, MO(2),  \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, LCA_T(KC_HOME), \
         MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
         MO(1), KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT,   KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [1] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______, \
-        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
-        KC_CAPS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
-        _______, RGB_TOG, RGB_M_P, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, _______,          KC_MUTE, KC_VOLD, \
-        _______, _______, _______,                            _______,                            _______, _______, KC_MPRV, KC_MPLY, KC_MNXT \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, \
+        KC_CAPS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_HOME, \
+        _______, RGB_TOG, RGB_M_P, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, _______,          _______, KC_END, \
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______ \
     ),
     [2] = LAYOUT(
        /* There are a few keys that can be made more Mac-like: brightness up/down (f1/f2), media previous (f7), play/pause (f8), and media next (f9)
           as well as volume controls mute (f10), down (f11) and up (f12). mission control (f3) and launchpad (f4) appear un-mappable, and the keyboard
           brightness keys are in layer 1 as shipped. */
-       _______,  KC_BRMD, KC_BRMU, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_POWER, _______, \
-       _______,  KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, M_ENROLL, KC_KP_SLASH, KC_KP_ASTERISK, _______, _______, _______, _______, \
-       _______,  KC_KP_MINUS, KC_KP_PLUS, KC_KP_ENTER, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______, _______, _______, \
-       _______,  KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_DOT, KC_KP_EQUAL, KC_KP_COMMA, _______, _______, _______, _______, _______, _______, \
-       _______, _______, _______,                            _______,                              _______, _______, _______, _______, _______ \
+       _______, KC_BRMD, KC_BRMU, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_POWER, _______, \
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+       _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______ \
     ),
     /*
     [X] = LAYOUT(
